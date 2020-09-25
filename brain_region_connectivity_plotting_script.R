8a4f645943b36978b58d8042716fe71b4c961a95
mmarquis <mmarquis89@gmail.com>
2020-09-25 14:24:51 -0400

    Developed a processing and plotting script for pre/post ratio analysis

--- brain_region_connectivity_plotting_script.R
+++ brain_region_connectivity_plotting_script.R
@@ -1,63 +1,135 @@
+
 library(neuprintr)
 library(hemibrainr)
 library(colormap)
+library(base)
+
+# ==================================================================================================
+# LOCAL FUNCTIONS
+# ==================================================================================================
+
+# Returns (uniqueROIs, segROIs)
+assign_ROIs <- function(pointsList, segList, roiList, roiMeshes) {
+  
+  # Determine which points belong to which ROIs
+  print('Assigning ROIs to points...')
+  pointROIs = vector(mode="character", nrow(pointsList))
+  for (iRoi in 1:length(roiList)){
+    currROI = roiList[iRoi]
+    currROI_points = pointsinside(pointsList, surf=roiMeshes[[iRoi]])
+    pointROIs[currROI_points] = currROI
+  }
+  pointROIs[pointROIs == ""] = "None"
+  pointsList$roiNames = pointROIs;
+  uniqueROIs = unique(pointsList$roiNames)
+  
+  # Determine which segments belong to which ROIs (going by most common point ROI)
+  print("Assigning ROIs to segments...")
+  segROIs = vector(mode="character", length(segList))
+  for (iSeg in 1:length(segList)){
+    segPoints = pointsList[segList[[iSeg]],]
+    currRoiNames = segPoints$roiNames
+    if (any(currRoiNames != "None")){
+      segROIs[iSeg] = names(sort(table(segPoints$roiNames[segPoints$roiNames != "None"])))[1]  
+    }else{
+      segROIs[iSeg] = "None"
+    }
+  }
+  return(list("uniqueROIs" = uniqueROIs, "segROIs" = segROIs))
+}
+
+# Returns (roiColors)
+assign_colors <- function(currData, uniqueROIs, cmapName, nShades){
+  print('Assigning colors to ROIs...')
+  cm = colormap(colormap=colormaps[[cMapName]], nshades=nShades)
+  if (cMapName == "bluered"){
+    cm = rev(cm)
+  }
+  roiColors = vector(mode="character")
+  for (iRoi in 1:length(uniqueROIs)){
+    if (uniqueROIs[iRoi] == "None"){
+      roiColors[iRoi] = "#000000"
+    }else{
+      roiColors[iRoi] = cm[1 + currData$pctPre[currData$roiName == uniqueROIs[iRoi]]]
+    }
+  }
+  
+  return(roiColors)# Return values
+}
+
+# ==================================================================================================  
 
 conn = neuprint_login()
-
-# Load query neuron
-queryNeuronID = "294436967"
-queryNeuron = neuprint_read_neurons(queryNeuronID)
-queryNeuron = queryNeuron[[1]]
-pointsList = queryNeuron$d
-segList = queryNeuron$SegList
 
 # Load list of brain region ROIs
 roiList = c('AL(R)',	'AVLP(R)',	'CA(R)', 'CRE(R)',	'EPA(R)',	'FLA(R)',	'GNG',	'GOR(R)',	'IB',
             'ICL(R)',	'IPS(R)',	'LAL(R)',	'LH(R)',	'PED(R)',	'PLP(R)',	'POC',	'PRW',	'PVLP(R)',
             'SAD', 'SCL(R)',	'SIP(R)',	'SLP(R)',	'SMP(R)',	'SPS(R)',	'VES(R)',	'WED(R)')
 
+# Load the meshes for each ROI
+print('Loading ROI meshes...')
+roiMeshes = list()
+for (i in 1:length(roiList)){
+  roiMeshes[[i]] = neuprint_ROI_mesh(roiList[i])  
+}
+
 # Load synapse count data
 df = read.csv(paste("C:\\Users\\Wilson Lab\\Google Drive\\Lab Work\\LH-DAN_connectomics_analysis", 
                     "\\prePost_synapse_count_data.csv", sep=""))
 
-# Select subset of synapse count data by current query neuron
-currData = df[df$bodyID == queryNeuronID,]
-
-# Create colormap and assign colors to each brain region
-cm = colormap(colormap=colormaps$viridis, nshades=100)
-
-# Open new plotting window
-nopen3d()
-par3d(windowRect=c(50, 50, 1000, 900))
-
-# Loop through and plot neuron segments for each ROI in the appropriate color 
-for (i in 1:length(roiList)){
-  currROI = roiList[i]
-  print(currROI)
-  currROI_mesh = neuprint_ROI_mesh(currROI)
-  currROI_points = pointsinside(pointsList, surf=currROI_mesh)
-  if (any(currROI_points)){
-    for (i in 1:length(segList)){
-      currX = pointsList$X[segList[[i]]]
-      currY = pointsList$Y[segList[[i]]]
-      currZ = pointsList$Z[segList[[i]]]
-      
-      inROI = currROI_points[segList[[i]][1]]
-      
-      if (inROI == TRUE) {
-        currCol = cm[currData$pctPre[currData$roiName == currROI]]
-        lines3d(currX, currY, currZ, col=currCol, lwd=2)
-      }
-    }
+queryNeuronIDs = c("328533761", "294436967", "792040520", "950229431")
+cMapNames = c("viridis", "plasma", "bluered")
+for (iCell in 1:length(queryNeuronIDs)){
+  
+  # Load query neuron
+  print('Fetching neuron data...')
+  queryNeuronID = queryNeuronIDs[iCell]
+  queryNeuron = neuprint_read_neurons(queryNeuronID)
+  queryNeuron = queryNeuron[[1]]
+  pointsList = queryNeuron$d
+  segList = queryNeuron$SegList
+  
+  # Select subset of synapse count data by current query neuron
+  currData = df[df$bodyID == queryNeuronID,]
+  
+  # Assign ROIs to all segments
+  output = assign_ROIs(pointsList, segList, roiList, roiMeshes)
+  uniqueROIs = output$uniqueROIs
+  segROIs = output$segROIs
+  
+  for (iPlot in 1:length(cMapNames)){
+    
+    # Create colormap and assign colors to each brain region
+    cMapName = cMapNames[iPlot]
+    roiColors = assign_colors(currData, uniqueROIs, cmapName, 100)
+    
+    # Open new plotting window
+    nopen3d()
+    par3d(windowRect=c(50, 50, 1000, 900))
+    
+    # Loop through and plot neuron segments for each ROI in the appropriate color 
+    print("Plotting lines...")
+    t1 = Sys.time()
+    for (iSeg in 1:length(segList)){
+      segRoiName = segROIs[iSeg];
+      currCol = roiColors[uniqueROIs == segRoiName]
+      currSegPoints = pointsList[segList[[iSeg]], ]
+      lines3d(currSegPoints$X, currSegPoints$Y, currSegPoints$Z, col=currCol, lwd=2)
+    } 
+    t2 = Sys.time()
+    print(t2 - t1)
+  
   }
 }
-
 # TODO: 
-#   Increase colormap visual contrast by scaling from 0-67 and/or choosing a different one
-#   Load all meshes into a list first and then refer back to them (for potential plotting later)
-#   Make sure the points that fall outside any of the brain regions (e.g. primary neurite) get plotted
 #   Allow for averaging the two PPM1201 neuron's brain region percentages together for plotting
-#   Figure out why the fuck I can't plot PPL201 without getting an error
-
-
-
+# 
+# # 
+# devList = rgl.dev.list()
+# sceneList = vector(mode="list", length(devList))
+# for (i in 1:length(devList)){
+#   print(i)
+#   rgl.set(devList[[i]])
+#   sceneList[[i]] = scene3d()
+}
+# save("sceneList", file="C:\\Users\\Wilson Lab\\Documents\\MATLAB\\R-code\\colormap_test_scenes_allTypes_full_range.RData")
